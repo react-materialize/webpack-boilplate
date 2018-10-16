d825b3034db8a4f204dcccbb26b68d975b2fc47e
Alexander Odell <alextrastero@gmail.com>
2018-10-16 11:25:56 +0200

    button

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Carousel } from '../../react-materialize/src';
+import { Button } from '../../react-materialize/src';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.testo = this.testo.bind(this);
-  }
-
-  testo(e) {
-    console.log(e.target.value);
   }
 
   render() {
     return (
-      <Carousel images={[
-        'https://picsum.photos/200/?random',
-        'https://picsum.photos/200/?random',
-        'https://picsum.photos/200/?random',
-        'https://picsum.photos/200/?random',
-        'https://picsum.photos/200/?random'
-      ]} />
+      <div>
+        <Button>Click</Button>
+      </div>
     )
   }
 }
